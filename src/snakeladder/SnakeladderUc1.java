c740afa6c53bbc1208890a92622b084162ca0758
Rutuja Jakwad <rutujajakwad585@gmail.com>
2021-09-26 22:07:44 +0530

    Added: Content of the message

---
+++ src/snakeladder/SnakeladderUc1.java
@@ -0,0 +1,11 @@
+package snakeladder;
+
+public class SnakeladderUc1 {
+	public static void main(String[] args) {
+		int playerOne = 0;
+		int startPosition = 0;
+		int winningSpot = 100;
+		System.out.println("The game is played with a single player at 0 th position");
+	}
+
+}
